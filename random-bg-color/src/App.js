edaf3be873feff0b4a8706355ce6bbcc89fac48b
Pradipkakde <kakde.pradip1@gmail.com>
2024-02-21 22:50:04 +0530

    add logic for change color

--- random-bg-color/src/App.js
+++ random-bg-color/src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
+
+const handleClickFunction=(e)=>{
+  const body= document.querySelector('body')
+  body.style.backgroundColor=getRandomcolor();
+  e.target.style.backgroundColor=getRandomcolor();
+};
+function getRandomcolor(){
+  let letters='0123456789ABCDEFG'
+  let color='#'
+  for(let i=0;i<6;i++){
+    color+=letters[Math.floor(Math.random()*16)]
+    
+  }
+  return color
+}
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <div className="container">
+     <h1>Random Background Color</h1>
+     <button className="btn" onClick={handleClickFunction}>CHANGE COLOR</button>
     </div>
+    </>
   );
 }
 
