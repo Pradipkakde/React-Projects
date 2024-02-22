bbd0b8352eb0f5148bf53a3cf5f7864e44a275cf
Pradipkakde <kakde.pradip1@gmail.com>
2024-02-22 21:20:25 +0530

    add assest

--- dark-light-mode/src/component/Navbar.js
+++ dark-light-mode/src/component/Navbar.js
@@ -2,8 +2,20 @@ import React from 'react'
 
 export default function Navbar() {
   return (
-    <div>
-      Navbar cre
+    <div className='navbar'>
+      <img src="" alt="" className='logo'/>
+      <ul>
+        <li>Home</li>
+        <li>Contacts</li>
+        <li>Features</li>
+        <li>About</li>
+      </ul>
+      <div className='search-box'>
+        <input type='text' placeholder='search'/>
+        <img src="" alt=""/>
+      </div>
+
+      <img src="" alt="" className="toggle-icon"/>
     </div>
   )
 }
