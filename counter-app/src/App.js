5108269a1aa61f8ac20917f42e13921f93f15f8d
Pradipkakde <kakde.pradip1@gmail.com>
2024-02-23 22:29:58 +0530

    add more css for smart counter

--- counter-app/src/App.js
+++ counter-app/src/App.js
@@ -5,11 +5,12 @@ function App() {
   let [setvalue,newvalue]=useState(0)
   return (
     <> 
-    <h1>This is My Counter Machine</h1>
+    <h1 id="moving"><marquee>This is My Counter Machine</marquee></h1>
     <center>
       <div id="counter-box"> <h3 id="content">{setvalue} </h3></div>
       <br/>
-      <button className="btn btn-info" onClick={()=>newvalue(setvalue+1)}>Count the counting</button>
+      <button className="btn mx-2" onClick={()=>newvalue(setvalue+1)}>INCREMENT</button>
+      <button className="btn" onClick={()=>newvalue(setvalue-1)}>DECREMENT</button>
       </center>
      
     
