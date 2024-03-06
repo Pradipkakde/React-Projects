d7826e625135f8257f77130072863f772dac8386
Pradipkakde <kakde.pradip1@gmail.com>
2024-03-06 15:08:17 +0530

    fetch the data from youtube json by api

--- myyoutube/src/Pages/Video/Video.js
+++ myyoutube/src/Pages/Video/Video.js
@@ -1,10 +1,14 @@
 import React from 'react'
 import './Video.css'
 import Playvideo from '../../Components/PlayVideo/Playvideo'
+import Recommended from '../../Components/Recommended/Recommended'
+
+
 export default function Video() {
   return (
     <div className='play-container'>
       <Playvideo/>
+      <Recommended/>
       
     </div>
   )
