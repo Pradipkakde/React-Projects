029376e299ce4a5377896de79f49a0a7f57a5878
Pradipkakde <kakde.pradip1@gmail.com>
2024-03-16 16:24:05 +0530

    create registration form

---
+++ registrationform/src/App.js
@@ -0,0 +1,12 @@
+import Multipleinputs from './Component/Forms/Multipleinputs';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+     <Multipleinputs/>
+    </div>
+  );
+}
+
+export default App;
